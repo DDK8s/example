b530a420a8c9c731c7b0a0d0b00693ee63ec969a
DKey <kaplan.daguzhiev@mail.ru>
2022-03-28 22:00:30 +0300

    new version of task with correct test

--- 8.go
+++ 8.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type element struct {
 	data int
@@ -14,27 +19,73 @@ type singleList struct {
 }
 
 func main() {
-	var sls []int
-	numbers := []int{7, 2, 3, 4, 5, 6, 7, 8, 9}
-	singList := initList()
-	singList.maxValue = 3
-	var setOfStacks []*element
+	// Условные новые данные.
+	data := enterNewData()
 
-	setOfStacks, sls = singList.Push(setOfStacks, singList, numbers, sls)
-	singList.Pop(sls)
-	setOfStacks = singList.popAt(setOfStacks, sls)
+	// Инициализирует связанный список с
+	// несколькими начальными значениями.
+	singleList := createLinkedList()
 
-	singList.showAllElements(setOfStacks)
-	//fmt.Print(sls)
+	// Добавляет элемент в setOfStacks.
+	singleList.Push(data)
 
+	// Удаляет последний элемент в связанном списке.
+	singleList.Pop()
+
+	// Выводит в терминал элементы из связанного списка.
+	singleList.showAllElements()
 
 }
 
-func initList() *singleList {
-	return &singleList{}
+// Функция вызывается в тесте для
+// сравнения []expected и []result
+
+func (s *singleList) TestingFor() []int{
+	current := s.head
+	var result []int
+	for current != nil {
+		result = append(result, current.data)
+		current = current.next
+	}
+	return result
 }
 
-func (s *singleList) addElementInCurrentStack (data int) int{
+func createLinkedList()  *singleList{
+	c := &element{
+		data: 3,
+		next: nil,
+	}
+
+	b := &element{
+		data: 2,
+		next: c,
+	}
+
+	a := &element{
+		data: 1,
+		next: b,
+	}
+
+	singleList := &singleList{a, 2, 1}
+	singleList.maxValue = 3
+	return singleList
+}
+
+func enterNewData() int {
+	fmt.Println("Enter data: ")
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	t := sc.Text()
+	data, err := strconv.Atoi(t)
+
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func (s *singleList) Push (data int) {
 	element := &element{
 		data: data,
 	}
@@ -48,123 +99,23 @@ func (s *singleList) addElementInCurrentStack (data int) int{
 		current.next = element
 	}
 	s.len++
-	return data
+
 }
 
-func (s *singleList) Push (setOfStacks []*element, singList *singleList, numbers []int, sls []int) ([]*element, []int){
-
-	for _, data := range numbers {
-		//sls = append(sls, data)
-		sls = addMinValue(sls, data)
-		if singList.len < singList.maxValue {
-			singList.addElementInCurrentStack(data)
-
-		}else if singList.len == singList.maxValue {
-			setOfStacks = append(setOfStacks, singList.head)
-			singList.head = nil
-			singList.len = 0
-			singList.addElementInCurrentStack(data)
-		}
-	}
-
-	setOfStacks = append(setOfStacks, singList.head)
-	return setOfStacks, sls
-}
-
-func (s *singleList) fillArray(numbers [6]int) [6]int{
-	for _, v := range numbers {
-		s.addElementInCurrentStack(v)
-	}
-	return numbers
-}
-
-func (s *singleList) Pop(sls []int) {
-		i := 0
-		current := s.head
-		for current.next != nil {
-			current = current.next
-			i++
-		}
-		deleteMinValue(sls, current.data)
-		s.removeByIndex(i)
-}
-
-// Удаляет элемент i из списка.
-func (s *singleList) removeByIndex(i int) {
-
+func (s *singleList) Pop() {
 	current := s.head
-	j := 0
-	for j < i-1 {
-		j++
+	for current.next.next != nil {
 		current = current.next
 	}
-	remove := current.next
-	current.next = remove.next
-	s.len--
-	return
+	current.next = nil
 }
 
-func (s *singleList) showCurrentStackElements() error {
-	if s.head == nil {
-		return fmt.Errorf("single list is empty")
-	}
+func (s *singleList) showAllElements() {
+
 	current := s.head
 	for current != nil {
 		fmt.Println(current.data)
 		current = current.next
 	}
-	return nil
-}
-
-func (s *singleList) showAllElements(setOfStacks []*element) {
-
-	for i := 0; i < len(setOfStacks); i++ {
-		current := setOfStacks[i]
-		for current != nil {
-			fmt.Println(current.data)
-			current = current.next
-		}
-	}
-}
-
-func (s *singleList) popAt(setOfStacks []*element, sls []int) []*element{
-
-	index := 5
-	s.head = setOfStacks[index / 3]
-	d := index % 3
-	switch d {
-		case 1:
-			s.removeByIndex(0)
-		case 2:
-			s.removeByIndex(1)
-		case 0:
-			s.Pop(sls)
-	default:
-		fmt.Println("error")
-		return nil
-	}
-
-	return setOfStacks
-}
-
-func addMinValue(sls []int, data int) []int{
-	if len(sls) == 0{
-		sls = append(sls, data)
-	}
-
-	if data < sls[len(sls)-1] {
-		sls = append(sls, data)
-	}
-	return sls
-}
-
-func deleteMinValue(sls []int, data int) {
-
-	for i, v := range sls {
-		if data == v {
-			sls = append(sls[:i], sls[i+1:]...)
-		}
-	}
-
 
 }
--- 8_test.go
+++ 8_test.go
@@ -1,36 +1,29 @@
 package main
 
 import (
+
 	"testing"
 )
 
-func TestPush(t *testing.T) {
+func TestTestingFor(t *testing.T) {
 	// Arrange.
-	var sls []int
-	singList := initList()
-	var setOfStacks []*element
-
-	expectedSet := []*element{
-		{data: 7, next: nil},
-		{data: 4, next: nil},
-		{data: 7, next: nil},
-
-	}
-
-	numbers := []int{7, 2, 3, 4, 5, 6, 7, 8, 9}
-	//expected := 2
+	singleList := singleList{}
+	expected := []int{1, 2, 3}
 
 	// Act.
-	result, sls := singList.Push(setOfStacks, singList, numbers, sls)
+	for v := range expected{
+		singleList.Push(expected[v])
+
+	}
+	result := singleList.TestingFor()
 
 	// Assert.
-	for v := range result {
-		for d := range expectedSet {
-			if result[v].data != expectedSet[d].data{
-				t.Errorf("Incorrect result. Expected %d, got %d", expectedSet[d].data, result[v].data)
-				return
-			}
+	for i := range expected {
+		if expected[i] != result[i]{
+
+			t.Errorf("Incorrect result. Expected %d, got %d", expected[i], result[i])
+			return
+
 		}
 	}
-
 }
